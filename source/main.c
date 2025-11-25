f8a91d7c0be35b848ccde3904e5d475a98c90925
Ziousss <johnsson_daniel@outlook.com>
2025-11-25 23:29:58 +0000

    new folders and test

---
+++ source/main.c
@@ -0,0 +1,17 @@
+#include "../include/lexer/tokenizer.h"
+#include "../include/lexer/readFile.h"
+
+int main (int argc, char **argv) {
+    if (argc > 3 || argc == 1) {
+        perror("You need 2 arguments");
+        return 0;
+    }
+
+    char *source = readFile(argv[1]);
+    if (source == NULL){
+        return 1;
+    }
+
+    Tokenstruct *token = lexicalAnalyzer(source);
+    free(source);
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+int main () {
+    int a, b;
+    a = 10;
+    b = 12;
+    
+    int c = a + b;
+    return c;
+}
