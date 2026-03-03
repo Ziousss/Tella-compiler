6f5f37a2d28b0b25ca6b057ed6a95c0d2c5efa3d
Ziousss <johnsson_daniel@outlook.com>
2026-03-03 18:18:49 +0000

    ifStmtIR added

---
+++ source/IntermediateRep/IR/ifStmtIR.c
@@ -0,0 +1,16 @@
+#include "../include/IntermediateRep/IRheader.h"
+
+//Will finish later wanna tart the machine code
+void ifStmtIR(ASTnode *ifStmt, IRContext *context){
+    ASTnode *condition = ifStmt->data.if_node.condition;
+    ASTnode *ifNode = ifStmt->data.if_node.if_branch;
+    ASTnode *elseNode = ifStmt->data.if_node.else_branch;
+
+    int error = context->errors;
+    Operand conditionOP = expressionIR(condition, context);
+    if(context->errors != error){
+        return;
+    }
+
+    
+}
