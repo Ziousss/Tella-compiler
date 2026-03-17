95765e93e681d4b37dd417caae550441ee1dae7f
Ziousss <johnsson_daniel@outlook.com>
2026-03-17 20:10:51 +0000

    Feat: added condJumpAS.c file to handle false jumps.

---
+++ source/assemblyInstr/Instr/condJumpAS.c
@@ -0,0 +1,11 @@
+#include "../include/assemblyInstr/assemblyInstrHeader.h"
+
+void condJumpAS(IRstruct *condJump, FILE *output, StackLayout *stack){
+    Operand condition = condJump->data.condition_jump.condition;
+    int target = condJump->data.condition_jump.target_label;
+
+    int offset = getOffset(condition, stack);
+    fprintf(output, "mov rax, [rbp %+d]\n", offset);
+    fprintf(output, "test rax, rax\n");
+    fprintf(output, "je L%d\n", target);
+}
